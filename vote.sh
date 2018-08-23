941a4c74cfc6cbfe50d4ad8226cb3ed9fb35c741
xavier.dutoit <xavier@tttp.eu>
2018-08-23 10:32:13 +0200

    made vote generic

--- vote.sh
+++ vote.sh
@@ -1,12 +1,31 @@
-echo "mepid,mep,result,group,identifier" > cards/92926.csv
-ag --nonumbers 92926 parlparse/data/mep_rollcall.csv >> cards/92926.csv
+# eg vote.sh 92926 2018-07-05
+if [ -z "$1" ]; then
+  echo "missing vote id";
+  echo "usage: vote.sh {voteid} {YYYY-MM-DD}"
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "missing vote date";
+  q "select * from parlparse/data/item_rollcall.csv where identifier=$1" -d, -O -H
+  echo "usage: vote.sh {voteid} {YYYY-MM-DD}"
+  exit 1
+fi
+echo "generating vote card for $1 voted on $2"
+
+echo "mepid,mep,result,group,identifier" > "cards/$1.csv"
+ag --nonumbers $1 parlparse/data/mep_rollcall.csv >> "cards/$1.csv"
+
 #deal with item_rollcall.csv
-q 'select * from parlparse/data/item_rollcall.csv where identifier=92926' -d, -O -H
-echo "{id:92926,date:"yyyy-mm-dd"} > cards/92926.json
+q "select identifier,date,report,desc,title,for,against,abstention from parlparse/data/item_rollcall.csv where identifier=$1" -d, -H | while IFS="," read identifier date report desc title vfor against abstention
+do 
+printf "{\"id\":$1,\n\"date\":\"$date\",\n\"report\":\"$report\",\n\"name\":\"CHANGE ME\",\n\"rapporteur\":\"RAPPORTEUR\",\n\"desc\":\"$desc\",\n\"for\":$vfor,\"against\":$against,\"abstention\":$abstention}\n" > "cards/$1.json"
+cat cards/$1.json
+done
 #cp parlparse/data/meps.csv data/
 
 # where they present or excused?
 
-q 'select * from parlpase/data/mep_attendance.csv where date="2018-07-05"' -d, -H -O > cards/92926.attendance.csv
-q 'select voteid,name,status,"manual",92926 as result from cards/92926.attendance.csv left join data/meps.csv on id=epid left join cards/92926.csv on mepid=voteid where result is null' -d, -H >> cards/92926.csv
+q "select * from parlparse/data/mep_attendance.csv where date='$2'" -d, -H -O > "cards/$1.attendance.csv"
+exit
+q "select voteid,name,status,'manual',$1 as result from cards/$1.attendance.csv left join data/meps.csv on id=epid left join cards/$1.csv on mepid=voteid where result is null" -d, -H >> "cards/$1.csv"
 
